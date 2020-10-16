489371ac48a0aba9d6d29f7e6cac8de0653f1b6e
Tom Bekor <tom.bekor@gmail.com>
2020-10-16 11:43:43 +0300

    Revert "site main"
    This reverts commit fd696dbc

--- Site/app.py
+++ Site/app.py
@@ -2,13 +2,9 @@ import os
 from flask import Flask, redirect
 import click
 
-PICS_FOLDER = os.path.join('Site', 'preprocess_plots')
-
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(SECRET_KEY='dev')
-app.config['UPLOAD_FOLDER'] = PICS_FOLDER
 
-full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'correlation_heatmap_patient.png')
 
 @app.after_request
 def add_header(r):
@@ -25,14 +21,12 @@ def add_header(r):
 
 @app.route('/')
 def home():
-    print(PICS_FOLDER)
-    print(full_filename)
     return redirect('/mipmlp/Home')
-
 
 from Site import site_service
 
 app.register_blueprint(site_service.bp)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- Site/site_service.py
+++ Site/site_service.py
@@ -1,13 +1,7 @@
-import io
-import os
-import service
-from zipfile import ZipFile
-from os.path import basename
-
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, send_file
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
-from werkzeug.utils import secure_filename
+
 
 bp = Blueprint('mipmlp', __name__, url_prefix='/mipmlp')
 
@@ -16,8 +10,8 @@ bp = Blueprint('mipmlp', __name__, url_prefix='/mipmlp')
 def home_page():
     message = None
     if request.method == 'POST':
-        otu_file = request.files['otu_file']
-        tag_file = request.files['tag_file']
+        otu_file = request.form['otu_file']
+        tag_file = request.form['tag_file']
         taxonomy_level = request.form['taxonomy_level']
         taxnomy_group = request.form['taxnomy_group']
         epsilon = request.form['epsilon']
@@ -26,55 +20,15 @@ def home_page():
         normalization = request.form['normalization']
         norm_after_rel = request.form['norm_after_rel']
 
-        otu_file.save("OTU.csv")
-        tag_file.save("TAG.csv")
-        params = {'taxonomy_level': taxonomy_level, 'taxnomy_group': taxnomy_group, 'epsilon': epsilon,
-                  'normalization': normalization, 'z_scoring': z_scoring, 'norm_after_rel': norm_after_rel,
-                  'std_to_delete': 0, 'pca': (PCA, 'PCA')}
-        # service.evaluate(params)
-        #
-        # # create a ZipFile object
-        # with ZipFile('sampleDir.zip', 'w') as zipObj:
-        #     # Iterate over all the files in directory
-        #     for folderName, subfolders, filenames in os.walk("Mucositis"):
-        #         for filename in filenames:
-        #             # create complete filepath of file in directory
-        #             filePath = os.path.join(folderName, filename)
-        #             # Add file to zip
-        #             zipObj.write(filePath, basename(filePath))
-        #     for folderName, subfolders, filenames in os.walk("preprocess_plots"):
-        #         for filename in filenames:
-        #             # create complete filepath of file in directory
-        #             filePath = os.path.join(folderName, filename)
-        #             # Add file to zip
-        #             zipObj.write(filePath, basename(filePath))
-
-        image1 = 'Site\preprocess_plots\correlation_heatmap_patient.png'
-
-        images_names = [
-            'Site\preprocess_plots\correlation_heatmap_patient.png',
-            'Site\preprocess_plots\Correlation_between_each_component_and_the_labelprognosistask.svg',
-            'Site/preprocess_plots/Correlation_between_each_component_and_the_labelprognosistask.svg',
-            'Site/preprocess_plots/correlation_heatmap_patient.png',
-            'Site/preprocess_plots/correlation_heatmap_bacteria.png',
-            'Site\preprocess_plots\Correlation_between_each_component_and_the_labelprognosistask.svg'
-        ]
-        # return send_file("sampleDir.zip", mimetype='zip', as_attachment=True,)
-
         error = None
+
 
         # input validation
 
         flash(error)
         if not error:
-            return render_template('home.html', active='Home', otu_file=otu_file, tag_file=tag_file,
-                                   taxonomy_level=taxonomy_level,
-                                   taxnomy_group=taxnomy_group, epsilon=epsilon, z_scoring=z_scoring, PCA=PCA,
-                                   normalization=normalization,
-                                   norm_after_rel=norm_after_rel,
-                                   images_names=images_names,
-                                   image1=image1)
 
+            return render_template('home.html', active='Home', otu_file=otu_file)
     return render_template('home.html', active='Home')
 
 
